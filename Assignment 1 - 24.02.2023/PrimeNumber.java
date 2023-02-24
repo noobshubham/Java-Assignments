34209b3eb6f4e500b1e6f8a6cfc2b22669701774
noobshubham <noobshubham65@gmail.com>
2023-02-24 20:08:01 +0530

    c: PrimeNumber

---
+++ Assignment 1 - 24.02.2023/PrimeNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter the Number: ");
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+
+        if (value )
+        
+    }
+}
